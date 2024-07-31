f6aa52e22c312f5f9a321dacbfd78ab603961050
abdul-maajid <amaajid0@gmail.com>
2024-07-31 22:44:58 +0200

    changes

--- src/api-utils/newAccountHandler.ts
+++ src/api-utils/newAccountHandler.ts
@@ -1,6 +1,27 @@
-const HeliusApiKey = "";
-const WebHookId = "";
+const HeliusApiKey = "f593a5d6-3ae5-499b-9650-55ab72f76403";
+const WebHookId = "c2040a3f-f494-41e6-a703-20b9fc124780";
 
 export const newAcccountSubscriptionHandler = async (newTrg: string) => {
+  try {
+    const response = await fetch(
+      `https://api.helius.xyz/v0/webhooks/${WebHookId}?api-key=${HeliusApiKey}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          webhookURL: "https://trades-webhook-start-pxwm.onrender.com/webhook",
+          transactionTypes: ["Any"],
+          accountAddresses: [newTrg],
+          webhookType: "rawDevnet",
+        }),
+      }
+    );
 
+    const data = await response.json();
+    console.log({ data });
+  } catch (e) {
+    console.error("error", e);
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,24 @@ import {
   handleCancelSubscription,
   handleNewSubscription,
 } from "./api-utils/subscritionHandler";
-import { tradeHandler } from "./api-utils/tradeHandler";
 
 const AppState = new Map<string, any>();
 
 export const app = async () => {
+  const keypair = Keypair.fromSecretKey(
+    new Uint8Array([
+      153, 251, 213, 43, 95, 145, 232, 205, 98, 119, 20, 238, 196, 110, 211, 41,
+      247, 89, 74, 192, 233, 67, 218, 233, 216, 209, 83, 110, 154, 66, 225, 35,
+      183, 226, 204, 186, 255, 78, 57, 189, 47, 58, 58, 171, 45, 12, 25, 88,
+      181, 73, 182, 76, 224, 138, 194, 78, 40, 228, 10, 67, 185, 200, 64, 66,
+    ])
+  );
+  const wallet = new Wallet(keypair);
+  const rpc = `https://devnet-rpc.shyft.to?api_key=fTjaNkS9PxIqTeKQ`;
+  const manifest = await dexterity.getManifest(rpc, true, wallet);
+
+  const trg = new PublicKey("2EjgK1Z2PPLj7pkUkfZbCdP5uo5UgScMPCZpNRi9rvtW");
+  const trader = new dexterity.Trader(manifest, trg);
 
   const server = Bun.serve({
     async fetch(req, server) {
@@ -24,8 +37,15 @@ export const app = async () => {
         case "/process-trade":
           break;
         case "/new-subscription":
+          response = await handleNewSubscription(
+            trader,
+            manifest,
+            searchParams.get("trg"),
+            AppState
+          );
           break;
         case "/cancel-subscription":
+          response = await handleCancelSubscription(AppState);
           break;
         default:
           break;
